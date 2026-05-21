f19ad23b43a53ffd21a065e2de9903e9dc91d8ca
Super User <root@docker.kawalei.com>
2026-05-21 18:41:53 +0100

    test: atualiza teste de widget para o ReflectaApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,16 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:reflecta_tmp/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Verifica se a tela inicial do Reflecta carrega corretamente', (WidgetTester tester) async {
+    // Constrói o aplicativo e renderiza o primeiro frame.
+    await tester.pumpWidget(const ReflectaApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verifica se os textos principais da interface estão presentes na tela.
+    expect(find.text('Reflecta'), findsOneWidget);
+    expect(find.text('Sua jornada interior'), findsOneWidget);
+    expect(find.text('NOVO CONSELHO'), findsOneWidget);
   });
 }
